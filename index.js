0c13770269083351c2b512fe012e0ed1f7c55391
+ <+>
2023-10-04 01:01:40 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,42 @@
+1 + 80;
+60 - 40;
+2 * 3.4;
+5.0 / 2.5;
+
+function add () {}
+function subtract () {}
+function multiply () {}
+function divide () {}
+
+function add() {
+    return "something";
+  }
+
+  function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+    return a / b;
+  }
+  function increment(n) {
+    return n + 1;
+  }
+  function decrement(n) {
+    return n - 1;
+  }
+  function makeInt(string) {
+    return parseInt(string, 10);
+  }
+  function preserveDecimal(string) {
+    return parseFloat(string);
+  }
+  
